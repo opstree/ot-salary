7f36bf384b814efd13904885f8c87bd9fb88318d
Priyanshi Chauhan <107524071+Cpriyanshi77@users.noreply.github.com>
2026-07-26 23:59:56 +0530

    Update SalaryApplicationTests.java

--- src/test/java/com/opstree/microservice/SalaryApplicationTests.java
+++ src/test/java/com/opstree/microservice/SalaryApplicationTests.java
@@ -1,9 +1,10 @@
+package com.opstree.microservice.salary;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SalaryApplicationTests {
-
     @Test
     void contextLoads() {
     }
